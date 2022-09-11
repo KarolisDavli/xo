b6751e7d5d70a5fe94a662f1e196738babf68517
Karolis Davli <kdavlijevas@gmail.com>
2022-09-11 16:11:57 +0200

    Check for winning combination

--- script.js
+++ script.js
@@ -2,15 +2,9 @@ const Player = (name, symbol) => {
   const playerName = name;
   const playerSymbol = symbol;
 
-  // const move = () => {
-  //   console.log(Game.activePlayer);
-  //   Game.setActivePlayer();
-  // };
-
   return {
     playerName,
     playerSymbol,
-    // move,
   };
 };
 
@@ -37,13 +31,11 @@ const Game = (() => {
       e.target.style.backgroundImage = `url('./img/${activePlayer.playerSymbol}mark.png')`;
       let PushIndex = e.target.getAttribute("index");
       gameBoard.splice(PushIndex, 1, activePlayer.playerSymbol);
-      console.log(gameBoard);
-      // Tuo paciu turiu idet simboliuka i gameBoard array
       setActivePlayer();
+      _checkWinner();
     } else if (e.target.innerHTML != "") {
       alert("Choose a different tile");
     }
-    console.log(e.target);
   };
 
   const renderGameBoard = () => {
@@ -59,21 +51,59 @@ const Game = (() => {
     });
   };
 
+  const _checkRows = () => {
+    row1 = [gameBoard[0], gameBoard[1], gameBoard[2]];
+    row2 = [gameBoard[3], gameBoard[4], gameBoard[5]];
+    row3 = [gameBoard[6], gameBoard[7], gameBoard[8]];
+
+    allRows = [row1, row2, row3];
+    console.log(allRows);
+
+    allRows.forEach((row) => {
+      if (row.every((i) => i == "x") || row.every((i) => i == "o")) {
+        console.log("linija");
+      }
+    });
+  };
+
+  const _checkColumns = () => {
+    col1 = [gameBoard[0], gameBoard[3], gameBoard[6]];
+    col2 = [gameBoard[1], gameBoard[4], gameBoard[7]];
+    col3 = [gameBoard[2], gameBoard[5], gameBoard[8]];
+
+    allCols = [col1, col2, col3];
+
+    allCols.forEach((col) => {
+      if (col.every((i) => i == "x") || col.every((i) => i == "o")) {
+        console.log("stulpelis");
+      }
+    });
+  };
+
+  const _checkDiagonal = () => {
+    diag1 = [gameBoard[0], gameBoard[4], gameBoard[8]];
+    diag2 = [gameBoard[2], gameBoard[4], gameBoard[6]];
+
+    allDiags = [diag1, diag2];
+
+    allDiags.forEach((diag) => {
+      if (diag.every((i) => i == "x") || diag.every((i) => i == "o")) {
+        console.log("diagnole");
+      }
+    });
+  };
+
+  const _checkWinner = () => {
+    _checkColumns();
+    _checkRows();
+    _checkDiagonal();
+  };
+
   return {
     activePlayer,
     renderGameBoard,
     setActivePlayer,
   };
-
-  // const playerMove = (activePlayer, event) => {
-  //   console.log(activePlayer);
-  //   console.log(event);
-  //   console.log(
-  //     `${activePlayer.playerName} is making a move. ${activePlayer.name} symbol is ${activePlayer.playerSymbol}`
-  //   );
-  //   console.log(activePlayer.target);
-  //   // setActivePlayer();
-  // };
 })();
 
 Game.renderGameBoard();
